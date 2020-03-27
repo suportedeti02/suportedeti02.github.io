e98fb5303d85fd324107db0ed354a52cbe49892e
suportedeti02 <suportedeti02@gmail.com>
2020-03-27 01:35:46 -0300

    removido imgs sem uso | modificado layout | adicionado novos itens

--- js/senha/btnSenha.js
+++ js/senha/btnSenha.js
@@ -2,10 +2,5 @@
 var click = window.document.getElementById('senha');
 
 function btnSenha() {
-    if(click.style.display == 'none')   {
-      click.style.display = 'block';
-    }
-    else{
-      click.style.display = 'none';
-    }
+
 }
